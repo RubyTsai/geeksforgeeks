2f0b9f376c3c9a8900279a596d1a3b5aab62acd9
RubyTsai <1791843+RubyTsai@users.noreply.github.com>
2017-09-27 13:28:04 -0700

    Update Print_Diagonally.cpp

--- Print_Diagonally.cpp
+++ Print_Diagonally.cpp
@@ -4,22 +4,37 @@
 #include <vector>
 using namespace std;
 
+vector<int> print(vector<int> vec, int size) {
+    vector<int> ret;
+    for (int a = 0; a < size; a++) 
+        for (int i = 0, j = a; j >= 0; i++, j--)  
+            ret.push_back(vec[i*size + j]);
+        
+    for (int a = 1; a < size; a++) 
+        for (int i = a, j = size-1; i < size; i++, j--)  
+            ret.push_back(vec[i*size + j]);
+
+    return ret;
+}
+
 int main() {
 	//code
 	int cases;
 	vector<int> vec;
+	vector<int> ret;
 	cin >> cases;
 	for (int i = 0; i < cases; i++) {
-	    int size, num;
+	    int size, nsize, num;
 	    cin >> size;
-	    cout << "size is: " << size << endl;
+	    nsize = size*size;
 	    vec.resize(size*size);
-	    for (int j = size; j < size*size; j++) {
+	    for (int j = 0; j < nsize; j++) {
 	        cin >> num;
 	        vec[j] = num;
 	    }
-	    for (int j = size; j < size*size; j++) 
-	        cout << vec[j];
+	    ret = print(vec, size);
+	    for (int j = 0; j < ret.size(); j++)
+	        cout << ret[j] << " ";
 	    cout << endl;
 	}
 	return 0;
